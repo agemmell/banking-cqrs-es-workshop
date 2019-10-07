608cf969230996fae5ba430e650c82fdaeabbfde
Alex Gemmell <agemmell@gmail.com>
2019-10-06 20:17:55 -0500

    First commit

---
+++ src/Console/GenerateCode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Workshop\Console;
+
+use EventSauce\EventSourcing\CodeGeneration\CodeDumper;
+use EventSauce\EventSourcing\CodeGeneration\YamlDefinitionLoader;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class GenerateCode
+ *
+ * @package Workshop\Console
+ */
+class GenerateCode extends Command
+{
+    protected static $defaultName = 'workshop:generate-code';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Generates EventSauce code files from YAML definition file')
+            ->setHelp('This command allows you to generate EventSauce code files from a YAML definition file')
+            ->addArgument('yaml', InputArgument::REQUIRED, 'YAML definition file to use')
+            ->addArgument('destination', InputArgument::REQUIRED, 'Destination file for the generated code');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $yamlPath = $input->getArgument('yaml');
+        $destination = $input->getArgument('destination');
+
+        $output->writeln($yamlPath);
+
+        $loader = new YamlDefinitionLoader();
+        $dumper = new CodeDumper();
+        $phpCode = $dumper->dump($loader->load($yamlPath));
+        file_put_contents($destination, $phpCode);
+    }
+}
